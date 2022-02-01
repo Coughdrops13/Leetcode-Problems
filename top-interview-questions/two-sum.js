92ebd33e95316267b1ec245a8688732465335f22
Connor Roberts <connoobets@gmail.com>
2022-02-01 11:12:02 -0700

    add solution with O(n) time complexity

--- top-interview-questions/two-sum.js
+++ top-interview-questions/two-sum.js
@@ -14,3 +14,25 @@ const twoSum = (nums, target) => {
 }
 
 console.log(twoSum(nums, target));
+
+// The following was copied from https://redquark.org/leetcode/0001-two-sum/
+
+
+// var twoSum = function (nums, target) {
+//   // Array to store the result
+//   result = [];
+//   // Map to store the difference and its index
+//   index_map = new Map();
+//   // Loop for each element in the array
+//   for (let i = 0; i < nums.length; i++) {
+//       let difference = target - nums[i];
+//       if (index_map.has(difference)) {
+//           result[0] = i;
+//           result[1] = index_map.get(difference);
+//           break;
+//       } else {
+//           index_map.set(nums[i], i);
+//       }
+//   }
+//   return result;
+// };
